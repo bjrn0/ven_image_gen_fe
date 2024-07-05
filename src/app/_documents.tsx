38da6bcabec250a3de1b70cb8b68223617e4302f
Ilya Roger <i.roger@p-c-m.de>
2024-07-05 04:11:05 +0300

    fix: minor changes

---
+++ src/app/_documents.tsx
@@ -0,0 +1,24 @@
+// pages/_document.js
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            http-equiv="Content-Security-Policy"
+            content="upgrade-insecure-requests"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
