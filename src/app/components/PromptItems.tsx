3e69b7ec3c11b9d7aff6e5508c96c62d66bcd90b
Ilya Roger <i.roger@p-c-m.de>
2024-07-05 05:41:10 +0300

    feat: styles changes to prompt items

--- src/app/components/PromptItems.tsx
+++ src/app/components/PromptItems.tsx
@@ -4,11 +4,11 @@ import { prompts } from "../data/prompts";
 import Image from "next/image";
 import { useAppDispatch, useAppSelector } from "../lib/hooks";
 import { setPromptText } from "../lib/slices/userSlice";
+import { useState } from "react";
 
 export const PromptItems = () => {
-  const imageId = useAppSelector((state) => state.user.generatedImageId);
   const dispatch = useAppDispatch();
-
+  const [selectedItem, setSelectedItem] = useState<string>("");
   return (
     <>
       <div className="w-60 overflow-scroll select-none mb-12">
@@ -16,13 +16,22 @@ export const PromptItems = () => {
           return (
             <div
               key={item.shortName}
-              className="relative w-full mb-4 cursor-pointer"
-              onClick={() => dispatch(setPromptText(item.prompt))}
+              className="relative w-full mb-4 cursor-pointer rounded-md group overflow-hidden"
+              onClick={() => {
+                setSelectedItem(item.shortName);
+                dispatch(setPromptText(item.prompt));
+              }}
             >
-              <p className="absolute bottom-0 text-center bg-gray-100 opacity-80 font-semibold w-full">
+              <p
+                className={`absolute bottom-0 text-center ${
+                  selectedItem === item.shortName &&
+                  "bg-orange-400 text-white opacity-100"
+                } bg-gray-100 opacity-80 font-semibold w-full z-50 `}
+              >
                 {item.shortName}
               </p>
               <Image
+                className="group-hover:scale-105 transition-all duration-300"
                 alt={item.shortName}
                 height="256"
                 width="256"
